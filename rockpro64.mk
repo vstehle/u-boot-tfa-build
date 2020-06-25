2b92cabbfa9ff56e7876b1f23558d792be3559e8
Grant Likely <grant.likely@arm.com>
2020-06-25 17:41:16 +0100

    Rockpro64: add target to flash SD
    This is a *terrible* hack, but it gets a target added for flashing the
    firmware to an SD. What is needed is a way to abstract platform specific
    flash targets so that each target can do what it needs.
    
    In the case of the rk3399 platforms, it would be best to create a GPT on
    the SD card with a couple of protected partitions for the firmware
    images. I'll look into doing that later.
    
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- rockpro64.mk
+++ rockpro64.mk
@@ -1,3 +1,9 @@
+# SPDX-License-Identifier: GPL-2.0+
+#
+# Copyright (C) Arm Limited, 2020
+#
+# Build config for RockPro64
+# Inspired by https://stikonas.eu/wordpress/2019/09/15/blobless-boot-with-rockpro64/
 
 DTB_TARGET := src/arm64/rockchip/rk3399-rockpro64-v2.dtb
 
@@ -8,3 +14,9 @@ TFA_DEPS   := dtb
 UBOOT_CONFIG := rockpro64-rk3399
 UBOOT_EXTRA  := BL31=${TFA_PATH}/build/${TFA_PLAT}/release/bl31/bl31.elf
 UBOOT_DEPS   := tfa
+
+FLASH_IMAGE := ${UBOOT_OUTPUT}/flash_image.bin
+
+rockpro64-sd:
+	sudo dd if=${UBOOT_OUTPUT}/idbloader.img of=${FLASH_DEVICE} seek=64 conv=fdatasync status=progress
+	sudo dd if=${UBOOT_OUTPUT}/u-boot.itb of=${FLASH_DEVICE} seek=16384 conv=fdatasync status=progress
