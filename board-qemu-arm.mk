65f7cd49aec7b0d40533bff15738dd00441abf14
Grant Likely <grant.likely@arm.com>
2020-08-12 17:42:57 +0100

    Create default qemu targets for fip and semihosting
    With this patch, by default both semihosting and nor_flash images will
    be built and can be conveniently run with either 'make qemu-fip' or
    'make qemu-semihosting'.
    
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- board-qemu-arm.mk
+++ board-qemu-arm.mk
@@ -14,13 +14,29 @@ tweakconfig:
 u-boot/all u-boot/u-boot.bin: tweakconfig
 endif
 
-$(FLASH_IMAGE): tfa/all
+all: $(FLASH_IMAGE)
+$(FLASH_IMAGE): tfa/all tfa/fip
 	dd if=/dev/zero of=$(FLASH_IMAGE) count=$$((64*1024*1024>>9))
 	dd if=$(TFA_OUTPUT)/$(TFA_PLAT)/release/bl1.bin of=$(FLASH_IMAGE) bs=4096 conv=notrunc
 	dd if=$(TFA_OUTPUT)/$(TFA_PLAT)/release/fip.bin of=$(FLASH_IMAGE) bs=4096 seek=64 conv=notrunc
 
-qemu: $(FLASH_IMAGE)
+all: semihosting
+semihosting: tfa/all
+	mkdir -p output
+	cp $(TFA_OUTPUT)/$(TFA_PLAT)/release/*.bin output
+	cp $(OPTEE_OUTPUT)/arm-plat-vexpress/core/tee-header_v2.bin output/bl32.bin
+	cp $(OPTEE_OUTPUT)/arm-plat-vexpress/core/tee-pager_v2.bin output/bl32_extra1.bin
+	cp $(OPTEE_OUTPUT)/arm-plat-vexpress/core/tee-pageable_v2.bin output/bl32_extra2.bin
+	cp $(UBOOT_OUTPUT)/u-boot.bin output/bl33.bin
+
+qemu-fip:
 	qemu-system-aarch64 -nographic -machine virt,secure=on -cpu cortex-a57 \
 		-no-acpi -smp 2 -m 1024 -bios $(FLASH_IMAGE) -d unimp \
 		-monitor null -serial stdio -serial tcp::5000,server,nowait
+
+qemu-semihosting:
+	cd output && qemu-system-aarch64 -nographic -machine virt,secure=on -cpu cortex-a57 \
+		-no-acpi -smp 2 -m 1024 -bios bl1.bin -d unimp \
+		-monitor null -serial stdio -serial tcp::5000,server,nowait \
+		-semihosting-config enable,target=native
 
