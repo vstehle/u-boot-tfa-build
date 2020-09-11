87180263920b270636e1038b111c97813c01c353
Grant Likely <grant.likely@arm.com>
2020-09-11 18:14:17 +0100

    rk3399: Add target to flash over USB
    'make flash-usb' will download the firmware to a rk3399 in maskrom mode
    via USB.
    
    Signed-off-by: Grant Likely <grant.likely@arm.com>

--- soc-rk3399.mk
+++ soc-rk3399.mk
@@ -4,12 +4,17 @@
 #
 # Build config for RockPro64
 # Inspired by https://stikonas.eu/wordpress/2019/09/15/blobless-boot-with-rockpro64/
+#
+# Flashing via USB requires rkdeveloptool. You can find it here:
+# rkdeveloptool: https://github.com/rockchip-linux/rkdeveloptool.git
 
 FLASH_IMAGE_DEPS := u-boot/all
 TFA_PLAT := $(CONFIG_SYS_SOC)
 OPTEE_PLATFORM := rockchip
 OPTEE_EXTRA += PLATFORM_FLAVOR=rk3399
 FLASH_IMAGE := $(UBOOT_OUTPUT)/flash_image.bin
+
+RKDEVELOPTOOL ?= rkdeveloptool
 
 ifeq ($(CONFIG_OPTEE),y)
 UBOOT_EXTRA += TEE=$(OPTEE_OUTPUT)/arm-plat-rockchip/core/tee.elf
@@ -25,3 +30,9 @@ sdimage:
 	/sbin/sgdisk -n 1:: $(SD_IMAGE)
 	dd if=${UBOOT_OUTPUT}/idbloader.img of=$(SD_IMAGE) seek=64
 	dd if=${UBOOT_OUTPUT}/u-boot.itb of=$(SD_IMAGE) seek=16384
+
+flash-usb: $(UBOOT_OUTPUT)/idbloader.img $(UBOOT_OUTPUT)/u-boot.itb
+	$(RKDEVELOPTOOL) db MiniLoaderAll.bin && sleep 0.25
+	$(RKDEVELOPTOOL) wl 0x40 $(UBOOT_OUTPUT)/idbloader.img
+	$(RKDEVELOPTOOL) wl 0x4000 $(UBOOT_OUTPUT)/u-boot.itb
+	$(RKDEVELOPTOOL) rd
